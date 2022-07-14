982fc40f99e0e8b3a12c8101faeb33f1b1b53da8
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-14 19:08:20 +0530

    14-07-22

--- Opps/polynomial.cpp
+++ Opps/polynomial.cpp
@@ -4,30 +4,30 @@ Properties :
 1. An integer (lets say A) which holds the coefficient and degrees. Use array indices as degree and A[i] as coefficient of ith degree.
 2. An integer holding total size of array A.
 Functions :
-1. Default constructor
-2. Copy constructor
-3. setCoefficient -
-This function sets coefficient for a particular degree value. If the given degree is greater than the current capacity of polynomial, increase the capacity accordingly and add then set the required coefficient. If the degree is within limits, then previous coefficient value is replaced by given coefficient value
-4. Overload "+" operator (P3 = P1 + P2) :
-Adds two polynomials and returns a new polynomial which has result.
-5. Overload "-" operator (P3 = p1 - p2) :
-Subtracts two polynomials and returns a new polynomial which has result
-6. Overload * operator (P3 = P1 * P2) :
-Multiplies two polynomials and returns a new polynomial which has result
-7. Overload "=" operator (Copy assignment operator) -
-Assigns all values of one polynomial to other.
-8. print() -
-Prints all the terms (only terms with non zero coefficients are to be printed) in increasing order of degree.
-Print pattern for a single term : <coefficient>"x"<degree>
-And multiple terms should be printed separated by space. And after printing one polynomial, print new line. For more clarity, refer sample test cases
-Input Format :
-Line 1 : N, total number of terms in polynomial P1
-Line 2 : N integers representing degree of P1 (separated by space)
-Line 3 : N integers representing coefficients of P1 (separated by space)
-Line 4 : M, total number of terms in polynomial P2
-Line 5 : M integers representing degree of P2 (separated by space)
-Line 6 : M integers representing coefficients of P2 (separated by space)
-Line 7 : Integer C, choice representing the function to be called (See main for more details)
+    1. Default constructor
+    2. Copy constructor
+    3. setCoefficient -
+    This function sets coefficient for a particular degree value. If the given degree is greater than the current capacity of polynomial, increase the capacity accordingly and add then set the required coefficient. If the degree is within limits, then previous coefficient value is replaced by given coefficient value
+    4. Overload "+" operator (P3 = P1 + P2) :
+    Adds two polynomials and returns a new polynomial which has result.
+    5. Overload "-" operator (P3 = p1 - p2) :
+    Subtracts two polynomials and returns a new polynomial which has result
+    6. Overload * operator (P3 = P1 * P2) :
+    Multiplies two polynomials and returns a new polynomial which has result
+    7. Overload "=" operator (Copy assignment operator) -
+    Assigns all values of one polynomial to other.
+    8. print() -
+    Prints all the terms (only terms with non zero coefficients are to be printed) in increasing order of degree.
+    Print pattern for a single term : <coefficient>"x"<degree>
+    And multiple terms should be printed separated by space. And after printing one polynomial, print new line. For more clarity, refer sample test cases
+    Input Format :
+    Line 1 : N, total number of terms in polynomial P1
+    Line 2 : N integers representing degree of P1 (separated by space)
+    Line 3 : N integers representing coefficients of P1 (separated by space)
+    Line 4 : M, total number of terms in polynomial P2
+    Line 5 : M integers representing degree of P2 (separated by space)
+    Line 6 : M integers representing coefficients of P2 (separated by space)
+    Line 7 : Integer C, choice representing the function to be called (See main for more details)
 Sample Input 1 :
 3
 1 3 5
