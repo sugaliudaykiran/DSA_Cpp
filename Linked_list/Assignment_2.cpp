f0e6478888bc23061d9be478e716858243660765
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-25 08:58:56 +0530

    25-07-22

--- Linked_list/Assignment_2.cpp
+++ Linked_list/Assignment_2.cpp
@@ -165,3 +165,190 @@ Node *skipMdeleteN(Node *head, int M, int N)
     }
     return head;
 }
+
+
+/****************************************************************
+        Swap two Nodes of LL
+ 
+    You have been given a singly linked list of integers along with two integers, 'i,' and 'j.'
+ Swap the nodes that are present at the 'i-th' and 'j-th' positions.
+
+Sample Input 2 :
+    2
+    10 20 30 40 -1
+    1 2
+    70 80 90 25 65 85 90 -1
+    0 6
+
+Sample Output 2 :
+    10 30 20 40 
+    90 80 90 25 65 85 70 
+
+*****************************************************************/
+
+Node *swapNodes(Node *head, int i, int j)
+{
+	//	ref - https://www.geeksforgeeks.org/swap-nodes-in-a-linked-list-without-swapping-data/    
+
+    if (head == NULL || head -> next == NULL || i == j)
+    {
+        return head;
+    }
+    
+    Node* prevX = NULL, *prevY= NULL, *curX= head, *curY= head;
+    int count = 0;
+    
+    while (curX && count != i)
+    {
+        prevX = curX;
+        curX = curX -> next;
+        count++;
+    }
+    
+    count = 0;
+    while (curY && count != j)
+    {
+        prevY = curY;
+        curY = curY -> next;
+        count++;
+    }
+     
+    if (curX == NULL || curY == NULL)	//	If any one is not in given range..
+    {
+        return head;
+    }
+    
+    if (prevX != NULL)	//	In case of head/tail and next be i and j..
+    {
+        prevX -> next = curY;
+    }
+    else
+    {
+		head = curY;
+    }
+    
+    if (prevY != NULL)	// //	In case of head/tail and next be j and i..
+    {
+        prevY -> next = curX;
+    }
+    else
+    {
+        head = curX;
+    }
+    						//	Both are at random position..
+	Node* temp = curY->next;
+    curY->next = curX->next;
+    curX->next = temp;
+    
+    return head;
+}
+
+
+
+/*
+         kReverse
+Given a singly linked list of integers, reverse the nodes of the linked list 'k' at a time and return its modified list.
+ 'k' is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of 'k,' 
+    then left-out nodes, in the end, should be reversed as well.
+
+
+Sample Input 2 :
+    2
+    1 2 3 4 5 -1
+    0
+    10 20 30 40 -1
+    4
+    
+Sample Output 2 :
+    1 2 3 4 5 
+    40 30 20 10 
+*/ 
+
+
+
+
+
+
+Node *kReverse(Node *head, int k)
+{
+    //Write your code here
+    if (head == NULL || k == 0)
+    {
+        return head;
+    }
+    
+    Node* cur = head, *next = NULL, *prev = NULL;
+    int count  = 0;
+    
+    while (cur && count < k)
+    {
+        next = cur -> next;
+        cur -> next = prev;
+        prev = cur;
+        cur = next;
+        
+        count++;
+    }
+    
+    if (next)  // or we can use "cur".. instead of "next"..
+    {
+        head -> next =  kReverse(next, k);	//	add recursive value to head -> next..
+    }
+    return prev;
+}
+
+
+
+
+
+/*
+
+Given a singly linked list of integers, sort it using 'Bubble Sort.'
+
+*/ 
+
+
+Node *bubbleSort(Node *head)
+{
+	// Write your code here
+    if (head == NULL || head -> next == NULL)
+    {
+        return head;
+    }
+    
+    Node* i = head;
+    while (i)
+    {
+        Node* j = head ;
+        Node*prev = head;
+        
+        while (j-> next)
+        {
+            Node*temp = j-> next;
+            if (j -> data > temp -> data)
+            {
+                if (j == head)
+                {
+                    j -> next = temp-> next;
+                    temp -> next = j;
+                    prev = temp;
+                    head = temp;
+                }
+                else
+                {
+                    j -> next = temp-> next;
+                    temp -> next = j;
+                    prev -> next = temp;
+                    prev = temp;
+                }
+            continue;  
+            }
+            prev = j;
+            j = j -> next;
+        }
+        i = i-> next;
+        
+    }
+    
+    return head;
+}
